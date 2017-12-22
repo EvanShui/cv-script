65df0e527ff575b7244580bc175b0ef8d683b95b
EvanShui <evanshui0@gmail.com>
2017-12-22 02:40:14 -0800

    successfully accepts the predictor and creates rect object

--- cv_script.py
+++ cv_script.py
@@ -14,6 +14,28 @@ ap.add_argument("-i", "--image", required=True,
 #maps the arg(shape-preidctor) to user input
 args = vars(ap.parse_args())
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape-predictor"])
 print(args)
+
+
+#initailizes dlib's face detector to locate the face then
+#initialize the predictor to locate facial landmarks
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+#preprocessing
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=500)
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+#converts image to greyscale
+#creates a bounding box of faces for images (seems like it's a list)
+rects = detector(gray, 1)
+
+#loop over face detections
+for (index, rect) in enumerate(rects):
+    #determine the facial landmarks for the face region, then convert the
+    #facial landmark (x, y)-coordinates to a NumPy array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+
+    print(shape)
